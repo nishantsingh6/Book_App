635ef44fb488abf558c48b57c34979fb58e5f247
Nishant Singh <142491474+nishantsingh6@users.noreply.github.com>
2025-05-16 15:55:25 +0530

    Update apiConnecter.js

--- FrontEnd/src/apiConnecter.js
+++ FrontEnd/src/apiConnecter.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-    baseURL:'http://localhost:4040/book'
+  baseURL: import.meta.env.VITE_API_URL, // Using Vite env variable 
 });
 
 API.interceptors.request.use((req) => {
